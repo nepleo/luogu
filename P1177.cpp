17295a37ab4faa858bf33470e39dc4acce950ec7
nepleo <1445694981@qq.com>
2020-03-23 15:47:11 +0800

    add P1177.cpp

--- P1177.cpp
+++ P1177.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-void quickSort(int *arr, int left, int right) {
-    int i = left, j = right;
-    int mid = arr[(i + j) / 2];
-    while (i <= j) {
-        while (arr[i] < mid) i++;
-        while (arr[j] > mid) j--;
-        if (i <= j) {
-            swap(arr[i], arr[j]);
-            i++;
-            j--;
-        }
+int Partition(long *arr, int left, int right) {
+    int temp = arr[left];
+    while (left < right) {
+        while (left < right && arr[right] > temp)
+            right--;
+        arr[left] = arr[right];
+        while (left < right && arr[left] <= temp)
+            left++;
+        arr[right] = arr[left];
     }
-    if (i < right) quickSort(arr, i, right);
-    if (left < j) quickSort(arr, left, j);
+    arr[left] = temp;
+    return left;
+}
+
+void quickSort(long *arr, int left, int right) {
+    if (left < right) {
+        int pos = Partition(arr, left, right);
+        quickSort(arr, left, pos - 1);
+        quickSort(arr, pos + 1, right);
+    }
 }
 
 int main() {
     long n;
     cin >> n;
-    int arr[n];
+    long arr[n];
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    quickSort(arr, 0, n);
+    quickSort(arr, 0, n - 1);
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
